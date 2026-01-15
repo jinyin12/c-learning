b6979ed42b30612cc1a5682bc8027f41db22e467
jinyin12 <2643810542@qq.com>
2026-01-15 11:23:50 +0800

    L1-006 连续因子循环做的，还是超时

---
+++ 1.15.1/1.15.1.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int start = 2;
+	int count = 0;
+	int arr[100] = { 0 };
+	int result[100] = { 0 };
+	int k = 0;
+	for (int count1=0; start * start <= n; start++)
+	{
+		for (int j=n,start1=start;; count1++)
+		{
+			if (j % start1 == 0)
+			{
+				arr[k++] = start1;
+				j = j / start1;
+				start1++;
+			}
+			else 
+			{
+				break;
+			}
+				
+		}
+		if (count1 > count)
+		{
+			for (int t = 0; t < k; t++)
+			{
+				result[t] = arr[t];
+			}
+			count = count1;
+		}		
+		count1 = 0;
+		k = 0;
+	}
+	if (count != 0)
+	{
+		printf("%d\n", count);
+		for (int i = 0; i < count; i++)
+		{
+			printf("%d", result[i]);
+			if (i != count - 1)
+				printf("*");
+		}
+	}
+	else printf("%d\n%d", 1, n);
+}
