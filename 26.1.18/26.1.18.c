7ec38b9cc316c9e6b6bae3978a0c90c7fff1c2d0
jinyin12 <2643810542@qq.com>
2026-01-18 12:14:24 +0800

    feat:L1-011 A-B升级版

--- 26.1.18/26.1.18.c
+++ 26.1.18/26.1.18.c
@@ -4,42 +4,17 @@ int main()
 {
 	char arr[10001] = { 0 };
 	gets(arr);
-	char arr1[256] = { 0 };
-	for (int i = 0; i < 256; i++)
+	int arr1[256] = { 0 };
+	char sign;
+	while(( sign=getchar())!= '\n')	
 	{
-		arr1[i] = '\t';
+		arr1[sign] = 1;
 	}
-	while (1)
+	for (int i = 0; arr[i] != '\0'; i++)
 	{
-		char sign;
-		sign=getchar();
-		if (sign == '\n')
-			break;
-		else 
-		{
-			int i = sign;
-			arr1[i] = sign;
-		}
-	}
-	int count = 1;
-	for (int i = 0;; i++)
-	{
-		if (arr[i] == '\0')
-		{
-			printf("%c",'\0');
-			break;
-		}
-		for (int j = 0;j<256; j++)
-		{
-			if (arr1[j] == arr[i])
-				count = 0;
-		}
-		if (count)
+		if (!arr1[arr[i]])
 		{
 			printf("%c", arr[i]);
 		}
-		count = 1;
-	}
-	
-	
+	}	
 }
